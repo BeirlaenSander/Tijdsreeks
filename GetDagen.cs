27b71bab2ba7b285ef2c125dd991687a16de1cce
Seeeender <sander.beirlaen@student.howest.be>
2022-10-06 10:13:37 +0200

    Azure Functions klaar

--- GetDagen.cs
+++ GetDagen.cs
@@ -9,9 +9,11 @@ using Microsoft.Extensions.Logging;
 using Newtonsoft.Json;
 using System.Data.SqlClient;
 using System.Collections.Generic;
+using tijdsreeks_groep2.Models;
+
 namespace MCT.Functions
 {
-    public class GetDagen
+    public class DagenFunctions
     {
         [FunctionName("GetDagen")]
         public async Task<IActionResult> Run(
@@ -42,8 +44,50 @@ namespace MCT.Functions
             }
             catch (System.Exception ex)
             {
-                throw ex;
+                log.LogError(ex.Message);
+                return new StatusCodeResult(500);
             }
         }
+
+        [FunctionName("GetVisitors")]
+        public async Task<IActionResult> GetVisitors([HttpTrigger(AuthorizationLevel.Anonymous, "get", Route = "visitors/{day}")] HttpRequest req,
+            string day,
+            ILogger log)
+            {
+                try
+                {
+                    string connectionString = Environment.GetEnvironmentVariable("ConnectionString");
+                    List<Visits> visits = new List<Visits>();
+                    using(SqlConnection connection = new SqlConnection(connectionString))
+                    {
+                        await connection.OpenAsync();
+                        using(SqlCommand command = new SqlCommand())
+                        {
+                            command.Connection = connection;
+                            command.CommandText = "SELECT TijdstipDag, AantalBezoekers FROM Bezoekers WHERE DagVanDeWeek = @dag";
+                            command.Parameters.AddWithValue("@dag",day);
+
+                            SqlDataReader reader = await command.ExecuteReaderAsync();
+                            while (await reader.ReadAsync())
+                            {
+                                var visit = new Visits();
+                                visit.Tijdstip=Convert.ToInt32(reader["TijdstipDag"]);
+                                visit.AantalBezoekers = Convert.ToInt32(reader["AantalBezoekers"]);
+                                visit.DagVanDeWeek = day;
+                                visits.Add(visit);
+                            }
+                        }
+                    }
+                    return new OkObjectResult(visits);
+                }
+                catch (System.Exception ex)
+                {
+                    log.LogError(ex.Message);
+                    return new StatusCodeResult(500);
+                }
+            }
     }
 }
+
+
+
---
+++ Models/Visits.cs
@@ -0,0 +1,9 @@
+using System;
+namespace tijdsreeks_groep2.Models;
+
+public class Visits
+{
+    public int Tijdstip { get; set; }
+    public int AantalBezoekers { get; set; }
+    public string DagVanDeWeek { get; set; }
+}
